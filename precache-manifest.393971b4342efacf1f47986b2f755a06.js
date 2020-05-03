f347ca15c75fd2736c8aede1f50aef16944598c5
Tatyana Pivgorodnyaya <peachpeachttt@gmail.com>
2020-05-03 17:12:12 +0300

    Updates

---
+++ precache-manifest.393971b4342efacf1f47986b2f755a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a44e8b9ba0f836267fcb59bba667cbc",
+    "url": "/testApp/index.html"
+  },
+  {
+    "revision": "ca1a3e68f45132dbfb88",
+    "url": "/testApp/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "557f4698cba1f2be7f93",
+    "url": "/testApp/static/css/main.6145c5f0.chunk.css"
+  },
+  {
+    "revision": "ca1a3e68f45132dbfb88",
+    "url": "/testApp/static/js/2.9da437a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testApp/static/js/2.9da437a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557f4698cba1f2be7f93",
+    "url": "/testApp/static/js/main.f5aa401f.chunk.js"
+  },
+  {
+    "revision": "bb7214b2ea2a9e810fdc",
+    "url": "/testApp/static/js/runtime-main.40cedda9.js"
+  }
+]);
